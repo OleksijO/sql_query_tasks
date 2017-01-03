71b4deed3de9e8f3bc45c71bab3edecbfd9ee11b
Oleksij Onysymchuk <oleksij.onysymchuk@gmail.com>
2017-01-03 19:19:11 +0200

    ALL DONE

--- sql_query_tasks.sql
+++ sql_query_tasks.sql
@@ -332,7 +332,6 @@ FROM (SELECT *
       WHERE b_show > 0 AND b_pic IS NOT NULL) AS shown_banners
 ORDER BY rate DESC;
 
-
 -- 20.	Написать запрос, показывающий самую старую публикацию на сайте (не важно – новость это или обзор).
 -- В результате выполнения запроса должно получиться:
 -- header 	date
@@ -382,7 +381,7 @@ FROM (
        FROM reviews
      ) AS united
 ORDER BY date
-LIMIT 1
+LIMIT 1;
 
 -- 21.	Написать запрос, показывающий список баннеров, URL которых встречается в таблице один раз.
 -- В результате выполнения запроса должно получиться:
@@ -391,6 +390,22 @@ LIMIT 1
 -- http://google.com 	6
 -- http://gismeteo.by 	9
 -- http://gismeteo.ru 	10
+
+SELECT
+  b_url,
+  b_id
+FROM banners AS primarie
+WHERE 1 = (SELECT COUNT(b_url)
+           FROM banners AS second
+           WHERE second.b_url = primarie.b_url);
+-- ИЛИ
+SELECT
+  b_url,
+  b_id
+FROM banners
+GROUP BY b_url
+HAVING COUNT(b_id) = 1
+ORDER BY b_id;
 
 -- 22.	Написать запрос, показывающий список страниц сайта в порядке убывания количества баннеров, расположенных на странице. Для случаев, когда на нескольких страницах расположено одинаковое количество баннеров, этот список страниц должен быть отсортирован по возрастанию имён страниц.
 -- В результате выполнения запроса должно получиться:
@@ -401,11 +416,53 @@ LIMIT 1
 -- Образцы договоров 	1
 -- Физическим лицам 	1
 
+SELECT
+  p_name,
+  COUNT(b_id) AS banners_count
+FROM pages
+  JOIN m2m_banners_pages ON pages.p_id = m2m_banners_pages.p_id
+GROUP BY m2m_banners_pages.p_id
+ORDER BY banners_count DESC, p_name ASC;
+
 -- 23.	Написать запрос, показывающий самую «свежую» новость и самый «свежий» обзор.
 -- В результате выполнения запроса должно получиться:
 -- header 	date
 -- Самолётом или поездом? 	2012-12-20 06:11:42
 -- Когда всё это кончится?! 	2012-12-12 06:31:13
+
+SELECT
+  header,
+  dt AS date
+FROM (
+       SELECT
+         n_header AS header,
+         n_dt     AS dt
+       FROM news
+     ) AS ordered_news
+WHERE dt = (SELECT MAX(dt)
+            FROM (
+                   SELECT
+                     n_header AS header,
+                     n_dt     AS dt
+                   FROM news
+                 ) AS ordered_news_1)
+UNION
+SELECT
+  header,
+  dt AS date
+FROM (
+       SELECT
+         r_header AS header,
+         r_dt     AS dt
+       FROM reviews
+     ) AS ordered_reviews
+WHERE dt = (SELECT MAX(dt)
+            FROM (
+                   SELECT
+                     r_header AS header,
+                     r_dt     AS dt
+                   FROM reviews
+                 ) AS ordered_reviews_1);
 
 -- 24.	Написать запрос, показывающий баннеры, в тексте которых встречается часть URL, на который ссылается баннер.
 -- В результате выполнения запроса должно получиться:
@@ -415,34 +472,91 @@ LIMIT 1
 -- 5 	http://google.by 	GOOGLE.BY
 -- 8 	http://habrahabr.ru 	HABRAHABR.RU
 
+SELECT
+  b_id,
+  b_url,
+  b_text
+FROM banners
+WHERE LOCATE(SUBSTR(b_url, 8), b_text) > 0
+
 -- 25.	Написать запрос, показывающий страницу, на которой размещён баннер с самым высоким отношением кликов к показам.
 -- В результате выполнения запроса должно получиться:
 -- p_name
 -- Юридическим лицам
+
+SELECT p_name
+FROM pages
+  JOIN m2m_banners_pages ON pages.p_id = m2m_banners_pages.p_id
+  JOIN (SELECT *
+        FROM banners
+        WHERE b_show > 0) AS showed_banners ON m2m_banners_pages.b_id = showed_banners.b_id
+ORDER BY showed_banners.b_click / showed_banners.b_click
+LIMIT 1;
 
 -- 26.	Написать запрос, считающий среднее отношение кликов к показам по всем баннерам, которые были показаны хотя бы один раз.
 -- В результате выполнения запроса должно получиться:
 -- AVG(`b_click`/`b_show`)
 -- 0.81224975
 
+SELECT AVG(`b_click` / `b_show`)
+FROM (SELECT *
+      FROM banners
+      WHERE b_show > 0) AS showed_banners;
+
 -- 27.	Написать запрос, считающий среднее отношение кликов к показам по баннерам, у которых нет графической части (поле `b_pic` равно NULL).
 -- В результате выполнения запроса должно получиться:
 -- AVG(`b_click`/`b_show`)
 -- 0.66000000
+
+SELECT AVG(`b_click` / `b_show`)
+FROM (SELECT *
+      FROM banners
+      WHERE b_show > 0 AND b_pic IS NULL) AS showed_banners;
 
 -- 28.	Написать запрос, показывающий количество баннеров, размещённых на страницах сайта верхнего уровня (у таких страниц нет родительских страниц).
 -- В результате выполнения запроса должно получиться:
 -- COUNT
 -- 4
 
+SELECT COUNT(b_id) AS COUNT
+FROM m2m_banners_pages
+  JOIN pages ON m2m_banners_pages.p_id = pages.p_id
+WHERE p_parent IS NULL;
+
 -- 29.	Написать запрос, показывающий баннер(ы), который(ые) показаны на самом большом количестве страниц.
 -- В результате выполнения запроса должно получиться:
 -- b_id 	b_url 	COUNT
 -- 1 	http://tut.by 	4
+
+SELECT
+  banners.b_id,
+  b_url,
+  COUNT(m2m_banners_pages.p_id) AS COUNT
+FROM banners
+  JOIN m2m_banners_pages ON banners.b_id = m2m_banners_pages.b_id
+GROUP BY b_id
+HAVING COUNT(m2m_banners_pages.p_id) = (SELECT MAX(sub_count)
+                                        FROM (SELECT COUNT(m2m_banners_pages.p_id) AS sub_count
+                                              FROM banners
+                                                JOIN m2m_banners_pages ON banners.b_id = m2m_banners_pages.b_id
+                                              GROUP BY m2m_banners_pages.b_id) AS sub_query_2);
 
 -- 30.	Написать запрос, показывающий страницу(ы), на которой(ых) показано больше всего баннеров.
 -- В результате выполнения запроса должно получиться:
 -- p_name 	COUNT
 -- Юридическим лицам 	3
 -- Схема проезда к офису 	3
+
+SELECT
+  p_name,
+  COUNT(m2m_banners_pages.b_id) AS COUNT
+FROM pages
+  JOIN m2m_banners_pages ON pages.p_id = m2m_banners_pages.p_id
+GROUP BY p_name
+HAVING COUNT(m2m_banners_pages.b_id) = (SELECT MAX(sub_count)
+                                        FROM (SELECT COUNT(m2m_banners_pages.b_id) AS sub_count
+                                              FROM pages
+                                                JOIN m2m_banners_pages ON pages.p_id = m2m_banners_pages.p_id
+                                              GROUP BY p_name)AS sub_query_2)
+ORDER BY p_name DESC;
 
